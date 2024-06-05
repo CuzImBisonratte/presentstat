7d315b34891f0429e894e0837e80180e04ba9fce
Konstantin Protzen <code@kondev.de>
2024-06-05 21:00:08 +0200

    💡 Add comments

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+// Modules
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 
+// Config
+const config = require('./config');
+
+// The controller routes (Served for the person who is controlling - mostly the host)
 const controller = require('./routes/controller');
 app.use('/ctrl', controller);
 
+// Live view routes (Live stream of the results)
 const live_view = require('./routes/live_view');
 app.use('/live', live_view);
 
+// Public routes (Served for all guests in the audience)
 const public = require('./routes/public');
 app.use('/public', public);
 
+
+// Start the server
 app.listen(config.PORT, () => {
     console.log('Server is running on port', config.PORT);
 });
