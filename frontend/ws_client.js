bfbd3bf44ac6493173bbfce0871fe4e875330f0a
Konstantin Protzen <code@kondev.de>
2024-06-05 22:06:59 +0200

    ✨ Add ws connector

--- frontend/ws_client.js
+++ frontend/ws_client.js
@@ -1,5 +1,11 @@
-const ws = new WebSocket('ws://{HOST}:{WS_PORT}');
+const WS_ADRESS = `ws://{HOST}:{WS_PORT}`;
+let ws = new WebSocket(WS_ADRESS);
 
 ws.onmessage = (message) => {
     console.log('Received message:', message.data);
+}
+
+// On connection error or close, try to reconnect
+ws.onclose = ws.onerror = () => {
+    for (let i = 0; i < 10; i++) setTimeout(() => location.reload(), 3000);
 }
