7676c14bd2d5a92503f2d32c4cf1e696de2a9e18
Konstantin Protzen <code@kondev.de>
2024-06-06 19:15:30 +0200

    ✨ Send question on registration

--- ws_server.js
+++ ws_server.js
@@ -4,6 +4,7 @@ const config = require('./config');
 
 const ws_server = new ws.Server({ port: config.WS_PORT });
 
+let current_questionset = null;
 ws_server.on('connection', (ws) => {
     ws.on('message', (message) => {
         if (!JSON.parse(message)) return;
@@ -13,10 +14,17 @@ ws_server.on('connection', (ws) => {
             case 'register':
                 if (!['controller', 'live_view', 'public'].includes(message.role)) return;
                 ws.role = message.role;
+                // Send current questionset to public client or live view
+                if (message.role === 'public' || message.role === 'live_view') {
+                    if (current_questionset) {
+                        ws.send(JSON.stringify({ msg_type: 'start_question', question: current_questionset.questions[current_questionset.current] }));
+                    }
+                }
                 break;
             case 'start_question':
                 if (ws.role !== 'controller') return;
-                const question = message.question.questions[message.question.current];
+                current_questionset = message.question;
+                const question = current_questionset.questions[message.question.current];
                 ws_server.clients.forEach((client) => {
                     if (client.role === 'public' || client.role === 'live_view') {
                         // Relay the question to public clients
